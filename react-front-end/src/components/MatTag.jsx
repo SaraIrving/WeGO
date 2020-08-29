50c3046050bdab2274d2fc507fe737dcc1c69f78
Sara <sarakirving@gmail.com>
2020-08-29 14:45:59 -0700

    added a onClick attribute to the MatTag to make the ClearIcon clickable, onClick function will be passed down via props

--- react-front-end/src/components/MatTag.jsx
+++ react-front-end/src/components/MatTag.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ClearIcon from '@material-ui/icons/Clear';
 
 // props.tag is a string that describes whichever tag was was clicked Ex: "outdoor", "hiking", "beginner"
+// props.onClick is a function which will remove the tag component when it is clicked
 
 export default function MatButton(props) {
   return (
 
-    <p>{props.tag}<ClearIcon /></p>
+    <p>{props.tag}<ClearIcon onclick={() => props.onClick}/></p>
   )
 };
