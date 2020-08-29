c6eb1de70902d38776676969956b70b136a7bea6
Sara <sarakirving@gmail.com>
2020-08-29 13:26:58 -0700

    Create an input textfield

--- react-front-end/src/components/MatInput.jsx
+++ react-front-end/src/components/MatInput.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
 
+// props.label: a string of whatever label you want to appear over the Input, ex: "Name"
 
 export default function MatInput(props) {
+
   return (
   
     <TextField
