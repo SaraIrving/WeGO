b5f3af88a233eac35c5eddabfaecddb6fc2ac9d7
Jeremy Huot <logflume@live.com>
2020-08-28 20:25:45 -0600

    Update db with new tags and activity_tags tables, re seed db, add component and state planning to project kickoff md

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,38 +1,45 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import Button from './components/Button';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       message: 'Click the button to load data!'
+//     }
+//   }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+//   fetchData = () => {
+//     axios.get('/api/users') // You can simply make your requests to "/api/whatever you want"
+//     .then((response) => {
+//       // handle success
+//       console.log(response.data) // The entire response from the Express API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+//       console.log(response.data.message) // Just the message
+//       this.setState({
+//         message: response.data[0].name
+//       });
+//     }) 
+//   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
-  }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>{ this.state.message }</h1>
+//         <button onClick={this.fetchData} >
+//           Fetch Data
+//         </button>        
+//       </div>
+//     );
+//   }
+// }
+
+
+
+export default function App(props) {
+  return(
+    <Button />
+  )
 }
-
-export default App;
