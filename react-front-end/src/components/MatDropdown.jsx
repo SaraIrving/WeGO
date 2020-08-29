7dfe49766d162eff17dad5a568da6b16dd4543cb
Sara <sarakirving@gmail.com>
2020-08-29 13:26:42 -0700

    create dropdown textfield that displays items based what is passed in via props

--- react-front-end/src/components/MatDropdown.jsx
+++ react-front-end/src/components/MatDropdown.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-// const myButton = Button
+import { TextField } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+
+
+// will use dropdown for: skill level, frequency 
+
+//props.label: a string that represents what you want the placeholder the dropdown to be
+// props.options: an array of the strings you want to have as the options in the dropdown list 
+// option refers to the option (as in state)
+
+
 
 export default function MatButton(props) {
+  const options = props.options
+
   return (
-    <Button />
+    <TextField
+          id="outlined-select"
+          select
+          label={props.label}
+          helperText={props.field}
+          variant="outlined"
+        >
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+        </TextField>
   )
 };
+
+
